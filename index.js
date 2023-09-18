93b2cb2ad92bc6f733e946271fb543dbc7e4f6ff
Rahul Panchal <127726141+rahulcodz@users.noreply.github.com>
2023-09-18 13:24:40 +0530

    misc

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8000",
+    origin: "https://chat-app-client-ivory.vercel.app/",
     methods: ["GET", "POST"],
   },
 })
